24f2dfafb9c56164f61c10f953f22f672c508973
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-10-07 18:09:51 +0200

    chore: Fix typo

--- nsd/nodejs/url.go
+++ nsd/nodejs/url.go
@@ -3,7 +3,7 @@ package nodejs
 // DownloadURL has two values either the stable url or the url for unofficial builds
 type DownloadURL string
 
-// Stable and Unofficial ar the two values for DownloadURL
+// Stable and Unofficial are the two values for DownloadURL
 const (
 	Stable     DownloadURL = "https://nodejs.org/dist/v%s/node-v%s-%s.%s"
 	Unofficial DownloadURL = "https://unofficial-builds.nodejs.org/download/release/v%s/node-v%s-%s.%s"
