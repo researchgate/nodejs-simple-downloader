33a00c34326191ed2470ea4f5634612960d6873b
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-10-19 15:09:24 +0200

    fix: Exit program with error code on error

--- nsd/cmd/root.go
+++ nsd/cmd/root.go
@@ -1,16 +1,24 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "nsd",
-	Short: "A simple cli downloader for nodejs, npm and yarn",
-	Long:  "",
+	Use:          "nsd",
+	Short:        "A simple cli downloader for nodejs, npm and yarn",
+	Long:         "",
+	SilenceUsage: true,
 }
 
 // Execute executes the root command.
 func Execute() error {
-	return rootCmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+		return err
+	}
+
+	return nil
 }
