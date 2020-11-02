b62bbea72625f7cc91fb651a4510ba3d261cce9c
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-11-02 15:11:00 +0100

    tests: Add tests for download

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/dustin/go-humanize v1.0.0
+	github.com/gorilla/mux v1.8.0
 	github.com/mholt/archiver/v3 v3.5.0
 	github.com/spf13/cobra v1.1.1
 )
---
+++ nsd/download/download_test.go
@@ -0,0 +1,56 @@
+package download
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestDownloadTgz(t *testing.T) {
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./testdata/")))
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	filePath, err := File(server.URL + "/basic.tar.gz")
+	if filePath == "" {
+		t.Errorf("File() returned no path")
+	}
+	defer os.Remove(filePath)
+
+	if err != nil {
+		t.Errorf("File() did error %s", err)
+	}
+
+	info, err := os.Stat(filePath)
+	if os.IsNotExist(err) || info.IsDir() {
+		t.Errorf("File() returned path to file, but file does not exist")
+	}
+}
+
+func TestDownloadZip(t *testing.T) {
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./testdata/")))
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	filePath, err := File(server.URL + "/basic.zip")
+	if filePath == "" {
+		t.Errorf("File() returned no path")
+	}
+	defer os.Remove(filePath)
+
+	if err != nil {
+		t.Errorf("File() did error %s", err)
+	}
+
+	info, err := os.Stat(filePath)
+	if os.IsNotExist(err) || info.IsDir() {
+		t.Errorf("File() returned path to file, but file does not exist")
+	}
+}
